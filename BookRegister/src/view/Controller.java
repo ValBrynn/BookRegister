3db0d5318f5269f426806880a445bea0cf36303f
ValBrynn <guardianofthecity34@gmail.com>
2017-10-13 16:31:57 +0200

    Controller ...

---
+++ BookRegister/src/view/Controller.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import model.CollectionOfBooks;
+
+
+/**
+ *
+ * @author FarHad
+ */
+public final class Controller {
+    
+    private View view;
+    private CollectionOfBooks model= new CollectionOfBooks();
+    
+//    public Controller(View view, CollectionOfBooks model)
+//    {
+//        this.view=view;
+//        this.model=model;
+//    }
+    
+    
+    
+    
+    exitPressed exitMenuItemhHandler= new exitPressed();
+         
+    
+    private class exitPressed implements EventHandler<ActionEvent>{
+    
+     @Override
+     public void handle(ActionEvent event) {
+           System.exit(0);
+        }
+    
+    
+    
+    }
+    
+    
+}
