e0db6de15904f56156f58968b4cbe50a6cbd16a9
Usermon <tahir_97@hotmail.com>
2017-10-13 22:06:46 +0200

    Model content

--- BookRegister/src/model/Model.java
+++ BookRegister/src/model/Model.java
@@ -5,12 +5,48 @@
  */
 package model;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author mac
  */
 public class Model {
     
-    private CollectionsOfBooks collectionOfBooks;
+    private CollectionOfBooks collectionOfBooks;
+    private CollectionOfBooksHelpWithSerialization ser;
+    
+    public Model(){
+        collectionOfBooks = new CollectionOfBooks();
+        ser = new CollectionOfBooksHelpWithSerialization();
+    }
+    
+    public void createFile(String filename) throws IOException{
+        ser.serializeToFile(filename);
+    }
+    
+    public void saveFile(String filename) throws IOException, ClassNotFoundException{
+        ser.deSerializeFromFile(filename);
+    }
+    
+    public void addBook(String isbn, String title, int edition, double price,
+            ArrayList <String> authors) {
+        Book addedBook = new Book(isbn,title,edition,price);
+        addAuthors(authors, addedBook);
+        collectionOfBooks.addBook(addedBook);
+    }
+    
+    private void addAuthors(ArrayList <String> authors, Book b){
+        for(String newAuthor: authors){
+            b.addAuthor(new Author(newAuthor));
+        }
+    }
+    
+    public void removeBook(){
+    }
+    
+    public void sortBy(){
+    }
     
 }
